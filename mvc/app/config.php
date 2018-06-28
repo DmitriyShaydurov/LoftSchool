a2a520f622bb69cbbf1a5584edb023af5353f6af
Dmitriy Shaydurov <dmitriy.shaydurov@gmail.com>
2018-06-28 13:31:10 +0700

    mvc+eloquent boilerplate

---
+++ mvc/app/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 28.06.2018
+ * Time: 10:24
+ */
+defined("DBDRIVER")or define('DBDRIVER','mysql');
+defined("DBHOST")or define('DBHOST','localhost');
+defined("DBNAME")or define('DBNAME','mvc');
+defined("DBUSER")or define('DBUSER','root');
+defined("DBPASS")or define('DBPASS','');
+
+defined("CHARSET")or define('CHARSET','utf8');
+defined("COLLATION")or define('COLLATION','utf8_general_ci');
+defined("PREFIX")or define('PREFIX','');
---
+++ mvc/app/models/DataBase.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 28.06.2018
+ * Time: 10:16
+ */
+
+namespace App\models;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class DataBase
+{
+    function __construct(){
+        $capsule = new Capsule;
+        $capsule->addConnection([
+            'driver'    => DBDRIVER,
+            'host'      => DBHOST,
+            'database'  => DBNAME,
+            'username'  => DBUSER,
+            'password'  => DBPASS,
+            'charset'   => CHARSET,
+            'collation' => COLLATION,
+            'prefix'    => PREFIX,
+        ]);
+        // Make this Capsule instance available globally via static methods...
+        $capsule->setAsGlobal();
+        // Setup the Eloquent ORM...
+        $capsule->bootEloquent();
+
+
+    }
+
+
+}
