de7f71c48886c4e76445a0bc206869aa8447c39f
Dmitriy Shaydurov <dmitriy.shaydurov@gmail.com>
2018-07-08 07:08:00 +0700

    home work 5 corrected

--- home-work-5/picture.php
+++ home-work-5/picture.php
@@ -5,8 +5,9 @@ require_once 'vendor/autoload.php';
 use Intervention\Image\ImageManagerStatic as Image;
 
 // configure with favored image driver (gd by default)
-Image::configure(array('driver' => 'imagick'));
+//Image::configure(array('driver' => 'imagick'));
 
 // and you are ready to go ...
-$image = Image::make('img/acc.jpg')->resize(300, 200);
-$img->save('public/bar.jpg', 60);
+$image = Image::make('acc.jpg')->widen(300);
+$image ->rotate(45);
+$image->save('img/ar.jpg', 60);
